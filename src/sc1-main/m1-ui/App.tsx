28a0662bd95a14e4cf38800cb42e161c2172dd84
vdukhovnik <vdukhovnik1@gmail.com>
2022-10-15 14:23:37 +0200

    feat: added AppBar from Mui. 15.10.2022

--- src/sc1-main/m1-ui/App.tsx
+++ src/sc1-main/m1-ui/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {HashRouter} from "react-router-dom";
 import {Main} from "./Main/Main";
+import ButtonAppBar from "./Main/ButtonAppBar";
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
+          <ButtonAppBar/>
           <Main/>
       </HashRouter>
     </div>
