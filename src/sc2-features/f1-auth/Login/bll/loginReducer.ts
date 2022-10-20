0b4a9bc69c04872c1a367bb9c8330884fda3d6be
karinaOl <karina.olkok@yandex.ru>
2022-10-20 11:06:29 +0300

    style: code formatting in loginReducer

--- src/sc2-features/f1-auth/Login/bll/loginReducer.ts
+++ src/sc2-features/f1-auth/Login/bll/loginReducer.ts
@@ -1,8 +1,8 @@
-import {AppThunk} from "../../../../sc1-main/m2-bll/store";
-import {authAPI, LoginParamsType} from "../dal/login-api";
-import {setProfileDataAC} from "../../../f2-profile/bll/profileReducer";
-import {handleAppError} from "../../../../utils/error-utils";
-import {setIsLoadingAC} from "../../../../sc1-main/m2-bll/appReducer";
+import { AppThunk } from "../../../../sc1-main/m2-bll/store";
+import { authAPI, LoginParamsType } from "../dal/login-api";
+import { setProfileDataAC } from "../../../f2-profile/bll/profileReducer";
+import { handleAppError } from "../../../../utils/error-utils";
+import { setIsLoadingAC } from "../../../../sc1-main/m2-bll/appReducer";
 
 const initialState = {
     isLoggedIn: false,
@@ -22,21 +22,24 @@ export const loginReducer = (
 
 export const loginAC = (value: boolean) => ({ type: "login/LOGIN", value } as const);
 
-export const loginTC = (data: LoginParamsType): AppThunk => (dispatch) => {
-    dispatch(setIsLoadingAC(true))
-    authAPI.login(data)
-        .then(res => {
-            console.log(res.data)
-            dispatch(setProfileDataAC(res.data))
-            dispatch(loginAC(true))
-        })
-        .catch(e => {
-            handleAppError(e, dispatch)
-        })
-        .finally(()=>{
-            dispatch(setIsLoadingAC(false))
-        })
-}
+export const loginTC =
+    (data: LoginParamsType): AppThunk =>
+    (dispatch) => {
+        dispatch(setIsLoadingAC(true));
+        authAPI
+            .login(data)
+            .then((res) => {
+                console.log(res.data);
+                dispatch(setProfileDataAC(res.data));
+                dispatch(loginAC(true));
+            })
+            .catch((e) => {
+                handleAppError(e, dispatch);
+            })
+            .finally(() => {
+                dispatch(setIsLoadingAC(false));
+            });
+    };
 
 export type LoginInitialStateType = typeof initialState;
 export type LoginActionType = ReturnType<typeof loginAC>;
