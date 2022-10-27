53c128eafa3fbf6cb0deafc9ad4687f56be3bff8
vdukhovnik <vdukhovnik1@gmail.com>
2022-10-27 13:00:25 +0200

    fix: Packs.tsx and PacksTable.tsx.

--- src/sc2-features/f3-packs/ui/Packs.tsx
+++ src/sc2-features/f3-packs/ui/Packs.tsx
@@ -1,72 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { MenuItem, Pagination, Select, SelectChangeEvent, Stack } from "@mui/material";
-import { useAppDispatch } from "../../../sc1-main/m2-bll/store";
-import { addPackTC, getPacksTC } from "../bll/packsReducer";
+import { useAppDispatch, useAppSelector } from "../../../sc1-main/m2-bll/store";
+import {
+    addPackTC,
+    changeCountOfPacksOnPageAC,
+    changeCurrentPageAC,
+    deletePackTC,
+    getPacksTC,
+} from "../bll/packsReducer";
 import FormControl from "@mui/material/FormControl";
 import Typography from "@mui/material/Typography";
 import { PacksTable } from "./PacksTable";
 import { Title } from "./Title/Title";
 import { SettingsBar } from "./SettingsBar/SettingsBar";
-import s from "./Packs.module.css";
+import style from "./Packs.module.css";
 
 export const Packs = () => {
     const dispatch = useAppDispatch();
+    const currentPage = useAppSelector((state) => state.packs.page);
+    const countOfPacksOnPage = useAppSelector((state) => state.packs.pageCount).toString();
 
-    const [currentCardsPerPage, setCurrentCardsPerPage] = useState("10");
-    const [currentPage, setCurrentPage] = React.useState(1);
     const cardsPerPage = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-    // const choosePage = (page: number) => {
-    //     const payload: GetCardsPackRequestParamsType = {
-    //         page,
-    //         pageCount: currentCardsPerPage,
-    //     };
-    //     // dispatch(getPacksTC(payload));
-    // };
-    const handleChange = (e: SelectChangeEvent) => {
-        setCurrentCardsPerPage(e.target.value);
-        const payload = {
-            pageCount: e.target.value,
-        };
-        // dispatch(getPacksTC(payload));
+    const changeCountOfCards = (e: SelectChangeEvent) => {
+        dispatch(changeCountOfPacksOnPageAC(+e.target.value));
+        dispatch(getPacksTC());
     };
 
-    const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
-        setCurrentPage(value);
+    const changeCurrentPage = (event: React.ChangeEvent<unknown>, value: number) => {
+        dispatch(changeCurrentPageAC(value));
     };
 
     const addPacks = () => {
         dispatch(addPackTC("TEST_FROM_DELETE"));
     };
 
+    const deletePacks = () => {
+        dispatch(deletePackTC("6356b38d65c36e000499fa36"));
+    };
+
     useEffect(() => {
         dispatch(getPacksTC());
-    }, [dispatch]);
+    }, [dispatch, currentPage]);
 
     return (
-        <div className={s.packs}>
+        <div className={style.packs}>
             <Title title={"Packs list"} buttonName={"Add new pack"} callback={addPacks} />
             <SettingsBar />
             <PacksTable />
-            {/*{pages.map((currentPage, index) => (*/}
-            {/*    <span key={index} onClick={() => choosePage(currentPage)}>*/}
-            {/*        {currentPage}*/}
-            {/*    </span>*/}
-            {/*))}*/}
-            <Stack spacing={2}>
-                <Typography>Current Page: {currentPage}</Typography>
-                <Pagination count={10} page={currentPage} onChange={handleChangePage} />
+            <Stack spacing={4}>
+                <Typography style={{ color: "dodgerblue", margin: "20px" }}>
+                    Current Page: {currentPage}
+                </Typography>
+                <Pagination style={{}} count={10} page={currentPage} onChange={changeCurrentPage} />
             </Stack>
             <FormControl sx={{ m: -5, minWidth: 40 }} size="small">
-                <Select value={currentCardsPerPage} onChange={handleChange}>
+                <span className={style.showSpanText}>Show</span>
+                <Select
+                    value={countOfPacksOnPage}
+                    onChange={changeCountOfCards}
+                    style={{ top: "2px" }}
+                    sx={{ top: "2px" }}
+                >
                     {cardsPerPage.map((page, index) => (
                         <MenuItem key={index} value={page}>
                             {page}
                         </MenuItem>
                     ))}
                 </Select>
+                <span className={style.spanCardsText}>Cards per Page</span>
             </FormControl>
-            {/*<span>Cards per Page</span>*/}
         </div>
     );
 };
--- src/sc2-features/f3-packs/ui/PacksTable.tsx
+++ src/sc2-features/f3-packs/ui/PacksTable.tsx
@@ -15,7 +15,8 @@ import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
 import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
 import SchoolRoundedIcon from "@mui/icons-material/SchoolRounded";
 import { PackType } from "../dal/packs-api";
-import { deletePackTC } from "../bll/packsReducer";
+import { deletePackTC, sortCardsCountAC } from "../bll/packsReducer";
+import style from "./Packs.module.css";
 
 export const PacksTable = () => {
     const packs = useAppSelector<PackType[]>((state) => state.packs.cardPacks);
@@ -25,15 +26,25 @@ export const PacksTable = () => {
         dispatch(deletePackTC("6356b38d65c36e000499fa36"));
     };
 
+    const sortCardsCountInPack = () => {
+        dispatch(sortCardsCountAC([...packs.sort((a, b) => a.cardsCount - b.cardsCount)]));
+    };
+
     return (
         <>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Cards</TableCell>
-                            <TableCell>Last Updated</TableCell>
+                            <TableCell onClick={() => {}}>
+                                Name<button className={style.sortButton}>▼</button>
+                            </TableCell>
+                            <TableCell onClick={sortCardsCountInPack}>
+                                Cards<button className={style.sortButton}>▼</button>
+                            </TableCell>
+                            <TableCell onClick={() => {}}>
+                                Last Updated <button className={style.sortButton}>▼</button>
+                            </TableCell>
                             <TableCell>Created by</TableCell>
                             <TableCell>Actions</TableCell>
                         </TableRow>
